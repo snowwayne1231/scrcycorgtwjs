6fa964fd0aff081c2c4bf9103d52098d9b3e220e
snowwayne1231 <snowwayne1231@gmail.com>
2023-03-26 21:06:45 +0800

    fix param to 2023

--- naihu_booking_v2.js
+++ naihu_booking_v2.js
@@ -15,17 +15,18 @@ const bookingTimes = [18, 19];
 
 const domain = 'scr.cyc.org.tw';
 const tpsiteMap = {'南港': '02', '內湖': '12'};
-const maxWorker = 5;
+const maxWorker = 2;
 const idleSeconds = 25;
 var timerMap = {};
 var penddingQueue = [];
 var penddingAjax = [];
+var year = 2023;
 // init
 checkls(tpsiteMap[siteName], bookingTimes);
 
 
 function pushQueue(tpsite, date, time, stage) {
-    var url = '../tp' + tpsite + '.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid=' + stage + '&QTime=' + time + '&PT=1&D=2022/' + date;
+    var url = '../tp' + tpsite + '.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid=' + stage + '&QTime=' + time + '&PT=1&D=' + year + '/' + date;
     console.log(`[場次] 加入排程 日期: ${date}, 時間: ${time}, 場地代號: ${stage}`);
     penddingQueue.push(url);
 }
@@ -41,15 +42,18 @@ function lauchBooking() {
         method: 'GET',
         success: (e) => {
             penddingAjax.splice(0, 1);
-            console.log(`[場次預訂] ${dataset} 已經回應`, e);
+            console.log(`[場次預訂] ${dataset} 已經回應, :: [預定場地中，請耐心等候！]`);
             if (penddingAjax.length < maxWorker) {
                 lauchBooking();
             }
         },
         error: (err) => {
-            console.log('err: ', err);
+            console.log('[場次預訂錯誤] err: ', dataset + String(err).substring(0, 60));
             penddingAjax.splice(0, 1);
-            window.open(url, '_blank');
+            // window.open(url, '_blank');
+            if (penddingAjax.length < maxWorker) {
+                lauchBooking();
+            }
         },
     });
 }
@@ -62,7 +66,7 @@ function checkls(tpsite= '12', registerTimes=[15,16,17,18], instant=false) {
     now.setSeconds(now.getSeconds() + (idleSeconds));
     const isBefore = now.getHours() == 23 && now.getMinutes() == 59;
     const isAfter = now.getHours() == 0 && now.getMinutes() <= 12;
-    const gapTime = isBefore || isAfter ? (instant ? 1:200) : idleSeconds * 1000;
+    const gapTime = isBefore || isAfter ? (instant ? 1:500) : idleSeconds * 1000;
     
 
     var aryMorning = [];
@@ -99,7 +103,7 @@ function ajaxCheckList(tpsite = '12', date = '04/24', page = 2, allowedTimes = [
     window.setTimeout(() => {
         console.log(`檢測日期: ${date}  頁數: ${page} 場次時段: ${allowedTimes.join(',')}`);
         return $.ajax({
-            url: `https://${domain}/tp${tpsite}.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D=2022/${date}&D2=${page}`,
+            url: `https://${domain}/tp${tpsite}.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D=${year}/${date}&D2=${page}`,
             method: 'GET',
             success: (cxt) => {
                 timerMap[`${tpsite}_${page}`] = null;
@@ -120,10 +124,12 @@ function ajaxCheckList(tpsite = '12', date = '04/24', page = 2, allowedTimes = [
                         return eqSt ? a.time - b.time : b.stage - a.stage;
                     });
                     res.map(r => { pushQueue(r.tpsite, r.date, r.time, r.stage) });
-                    if (gapTime <= 100000) {
+                    if (gapTime <= 2000) {
                         new Array(maxWorker).fill(0).map(i => {
                             lauchBooking();
                         });
+                    } else {
+                        lauchBooking();
                     }
                 } else {
                     console.log(`場次時段 [ ${allowedTimes.join(',')} ] 未發現可定場次`);
@@ -133,6 +139,7 @@ function ajaxCheckList(tpsite = '12', date = '04/24', page = 2, allowedTimes = [
                 return false;
             },
             error: (evt) => {
+                timerMap[`${tpsite}_${page}`] = null;
                 if (penddingQueue.length == 0) {
                     console.log('Timeout!!!. Just do it!!!');
                     const stages = [87, 1074, 88, 86];
@@ -143,14 +150,15 @@ function ajaxCheckList(tpsite = '12', date = '04/24', page = 2, allowedTimes = [
                         });
                     });
                     res.map(r => { pushQueue(r.tpsite, r.date, r.time, r.stage) });
-                    new Array(maxWorker).fill(0).map(i => {
-                        lauchBooking();
-                    });
+                    // new Array(maxWorker).fill(0).map(i => {
+                    //     lauchBooking();
+                    // });
+                    lauchBooking();
                 }
                 var now = new Date();
                 checkls(tpsite, allowedTimes, now.getHours() + now.getMinutes() == 0);
             },
-            timeout: 5500,
+            timeout: 15 * 1000,
         });
     }, gapTime);
 }
